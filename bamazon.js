3915553aa85ea8b634e838d8602d5c098ac8b4c6
Aram <avalbaro@gmail.com>
2017-11-20 01:16:06 -0800

    got table to display but amounts are not decreasing

--- bamazon.js
+++ bamazon.js
@@ -6,13 +6,63 @@ var connection = mysql.createConnection({
 	port: 3306,
 	user: "root",
 	password: "Wesley12",
-	database: "bamazon"
+	database: "bamazon_db"
 });
 
 connection.connect(function(err){
 	if (err) throw err;
 	console.log("aliens have abducted me!");
-	// afterConnection();
+	makeTable();
 });
 
-module.exports = connection;
+var makeTable = function(){
+	connection.query("SELECT * FROM products", function(err, res){
+			for(var i=0; i<res.length; i++){
+				console.log(res[i].item_id+" || "+res[i].product_name+" || "+res[i].department_name+" || "+res[i].price+" || "+res[i].stock_quantity+"\n");
+			}
+	promptCustomer(res);
+	})
+};
+
+
+var promptCustomer = function(res){
+	inquirer.prompt([{
+		type: 'input',
+		name: 'choice',
+		message: "What would you like to buy?"
+	}]).then(function(answer){
+		var correct = false;
+		// loop through response
+		for(var i = 0; i<res.length; i++){
+			if (res[i].product_name == answer.choice){
+				// if their response is true then we will change correct to true and change the id
+				correct = true;
+				var product=answer.choice;
+				var id=i;
+				inquirer.prompt({
+					type: "input",
+					name: "quant",
+					message: "How may would you like?",
+					validate: function (value){
+						if (isNaN(value)==false){
+							return true;
+						} else {
+							return false;
+						}
+					}
+				}).then(function(answer){
+					if((res[id].stock_quantity-answer.quant)>0){
+						connection.query("UPDATA products SET stock_quantity='"+ (res[id].stock_quantity-answer.quant)+"' WHERE product_name='" + product + "'", function(err, res2){
+							console.log("Purchase Confirmed!");
+							makeTable();
+						})
+					} else {
+						console.log("Unable to proceed. Please make a valid selection!");
+						promptCustomer(res);
+					}
+				})
+			}
+		}
+	})
+}
+// module.exports = connection;
